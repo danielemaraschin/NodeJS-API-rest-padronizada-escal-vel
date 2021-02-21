011d4095951a0db887b73799b341903552cc742c
danielemaraschin <danikmaraschin@gmail.com>
2021-02-21 18:58:53 +0100

    doing the conexion with mysql

---
+++ api-petshop/api/banco-de-dados/index.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+
+const instancia = new Sequelize(
+    'petshop',  //nome db
+    'roots',    //usuario mysql
+    '123456',   //senha de teste
+    {             //objeto para passar outras config na nossa conexão
+        host: '127.0.0.1',        //ipe da maquina q estamos usando
+        dialect: 'mysql'
+    }
+)
+
+module.exports = instancia
