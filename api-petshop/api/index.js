c11d0fc8c922086b72645e3972a2524fe3744220
danielemaraschin <danikmaraschin@gmail.com>
2021-02-21 18:48:11 +0100

    working with bodyParser.json

--- api-petshop/api/index.js
+++ api-petshop/api/index.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+
+//bodyParser é o plug-in para nosso app
+app.use(bodyParser.json())
 
 app.listen(3000, () => console.log('A API está funcionando'))
