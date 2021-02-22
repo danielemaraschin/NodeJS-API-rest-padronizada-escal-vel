9e97a6f554bd78ff61608fb0ae637bdd84f1c9ef
danielemaraschin <danikmaraschin@gmail.com>
2021-02-22 12:01:53 +0100

    working with porta:3000

--- api-petshop/api/index.js
+++ api-petshop/api/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const config = require('config')
 
 
 //bodyParser é o plug-in para nosso app
 app.use(bodyParser.json())
 
-app.listen(3000, () => console.log('A API está funcionando'))
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando'))
