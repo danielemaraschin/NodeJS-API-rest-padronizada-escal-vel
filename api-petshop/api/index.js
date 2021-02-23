9da4d4d4e5f90a5b3d86bfabbdc97f23382d12d0
danielemaraschin <danikmaraschin@gmail.com>
2021-02-23 20:41:11 +0100

    working with app use and routes

--- api-petshop/api/index.js
+++ api-petshop/api/index.js
@@ -7,6 +7,6 @@ const config = require('config')
 //bodyParser é o plug-in para nosso app
 app.use(bodyParser.json())
 
-
+app.use('api/fornecedores', roteador)
 
 app.listen(config.get('api.porta'), () => console.log('A API está funcionando'))
