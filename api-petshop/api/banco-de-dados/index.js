a3248da2be57f4256d4959c7f9e25699aa2de6b4
danielemaraschin <danikmaraschin@gmail.com>
2021-02-27 19:22:15 +0100

    working with db

--- api-petshop/api/banco-de-dados/index.js
+++ api-petshop/api/banco-de-dados/index.js
@@ -4,8 +4,8 @@ const config = require('config')
 
 const instancia = new Sequelize(
     config.get('mysql.banco-de-dados'),  //nome db
-    config.get(' mysql.usuario'),    //usuario mysql
-    config.get(' mysql.senha'),   //senha de teste
+    config.get('mysql.usuario'),    //usuario mysql
+    config.get('mysql.senha'),   //senha de teste
     {             //objeto para passar outras config na nossa conexão
         host: config.get('mysql.host'),        //ipe da maquina q estamos usando
         dialect: 'mysql'        //qual eh o tipo de db que está trabalhando
