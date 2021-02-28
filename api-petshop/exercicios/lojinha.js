42092909023eef0e149fbad699e8d23f7ef3dc9b
danielemaraschin <danikmaraschin@gmail.com>
2021-02-28 11:02:32 +0100

    exercise without RESful

---
+++ api-petshop/exercicios/lojinha.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+
+const produtos = [
+  { nome: 'Bola de Basquete', categoria: 'esportes' },
+  { nome: 'Bicicleta', categoria: 'esportes' },
+  { nome: 'Batalha Naval', categoria: 'jogos-de-mesa' }
+]
+
+const produtosEmJson = JSON.stringify(produtos)
+
+app.use('produto’, (requisicao, resposta) => {
+  resposta.send(produtosEmJson)
+})
