270340142e89e9fe43bf0502c2c3af14fe0d40bb
danielemaraschin <danikmaraschin@gmail.com>
2021-03-01 17:01:06 +0100

    exercise: get

--- api-filmes/index.js
+++ api-filmes/index.js
@@ -4,5 +4,13 @@ const bodyParser = require('body-parser')
 //bodyParser é o plug-in para nosso app
 app.use(bodyParser.json())
 
+app.get(‘/api/filmes’, (requisicao, resposta) => {
+    const filmes = [
+        { nome: 'Os Vingadores 3' },
+        { nome: 'Destacamento Blood' },
+        { nome: 'Pantera Negra' }
+      ] 
+      
+})
             
 app.listen(3000, () => console.log('A API está funcionando e aceitando requisições'))
