571e63f85ec2476ec1ed2a03abebda01d73de0de
danielemaraschin <danikmaraschin@gmail.com>
2021-03-01 15:32:52 +0100

    working with listen 3000

--- api-filmes/index.js
+++ api-filmes/index.js
@@ -3,3 +3,6 @@ const app = express()                   //precisamos de uma instancia do express
 const bodyParser = require('body-parser')
 //bodyParser é o plug-in para nosso app
 app.use(bodyParser.json())
+
+            
+app.listen(3000, () => console.log('A API está funcionando e aceitando requisições'))
