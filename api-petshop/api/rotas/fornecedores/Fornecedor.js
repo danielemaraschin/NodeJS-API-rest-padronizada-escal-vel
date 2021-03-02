eab9926f2a3b8d76ac21475c8f8983c38531d55f
danielemaraschin <danikmaraschin@gmail.com>
2021-03-02 11:05:15 +0100

    creating new class, constructor, this. , ...

--- api-petshop/api/rotas/fornecedores/Fornecedor.js
+++ api-petshop/api/rotas/fornecedores/Fornecedor.js
@@ -1,7 +1,17 @@
 class Fornecedor {
+//METODO constructor 'constrói a classe' - parametro pode ser um objeto com os dados da classe (do fornecedor)
+    constructor({id, empresa, email, categoria, dataCriacao, dataAtualizacao, versao }){ // dentro desse método, devemos pegar cada valor desse deve ser passado para a propr da instancia
+        this.id = id;
+        this.empresa = empresa;
+        this.email = email;
+        this.categoria = categoria;
+        this.dataCriacao = dataCriacao;
+        this.dataAtualizacao = dataAtualizacao;
+        this.versao = versao;
 
-    
+    }
 
+    })
 }
 
 
--- api-petshop/api/rotas/fornecedores/index.js
+++ api-petshop/api/rotas/fornecedores/index.js
@@ -1,11 +1,18 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
-                                                        //metodo async-await pq é funcao de promessa
-roteador.use('/', async (requisicao, resposta) => {     //async antes de declarar a funcao
+const Fornecedor = require('./Fornecedor')
+
+                                                       //metodo async-await pq é funcao de promessa
+roteador.get('/', async (requisicao, resposta) => {     //async antes de declarar a funcao
     const resultados = await TabelaFornecedor.listar()  //funcao de promessa (assim como na funcao de criar tabelas)
     resposta.send(
         JSON.stringify(resultados)
     )                                
-})                                                      
+})       
+
+
+roteador.post('/', (requisicao, resposta) => {
+    const dadosRecebidos = requisicao.body
+})
 
 module.exports = roteador
