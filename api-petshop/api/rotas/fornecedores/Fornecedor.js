589d26cb56f20e7db9573ba6c5d46b1e4fd19ec8
danielemaraschin <danikmaraschin@gmail.com>
2021-03-02 11:29:29 +0100

    Conecting with db

--- api-petshop/api/rotas/fornecedores/Fornecedor.js
+++ api-petshop/api/rotas/fornecedores/Fornecedor.js
@@ -1,5 +1,7 @@
+const TabelaFornecedor = require('./TabelaFornecedor')
+
 class Fornecedor {
-//METODO constructor 'constrói a classe' - parametro pode ser um objeto com os dados da classe (do fornecedor)
+                                                                                            //METODO constructor 'constrói a classe' - parametro pode ser um objeto com os dados da classe (do fornecedor)
     constructor({id, empresa, email, categoria, dataCriacao, dataAtualizacao, versao }){ // dentro desse método, devemos pegar cada valor desse deve ser passado para a propr da instancia
         this.id = id;
         this.empresa = empresa;
@@ -8,10 +10,12 @@ class Fornecedor {
         this.dataCriacao = dataCriacao;
         this.dataAtualizacao = dataAtualizacao;
         this.versao = versao;
+    }
+
+    criar(){
 
     }
 
-    })
 }
 
 
