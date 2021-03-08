7a103bf044e029df83eabb2d61117d37b5384276
danielemaraschin <danikmaraschin@gmail.com>
2021-03-08 11:55:08 +0100

    exercise with push

--- api-games/index.js
+++ api-games/index.js
@@ -3,3 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
+
+
+const jogosFavoritos = new Array ()
+
+jogosFavoritos.push('super mário');
+
+console.log(jogosFavoritos)
