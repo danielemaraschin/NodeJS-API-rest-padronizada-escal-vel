17acd8eaeea9d95d725cc374af03ec615c4be5be
danielemaraschin <danikmaraschin@gmail.com>
2021-03-11 09:55:57 +0100

    adding status

--- api-petshop/api/rotas/fornecedores/index.js
+++ api-petshop/api/rotas/fornecedores/index.js
@@ -5,6 +5,7 @@ const Fornecedor = require('./Fornecedor')
                                                        //metodo async-await pq é funcao de promessa
 roteador.get('/', async (requisicao, resposta) => {     //async antes de declarar a funcao
     const resultados = await TabelaFornecedor.listar()  //funcao de promessa (assim como na funcao de criar tabelas)
+    resposta.status(200) //padrao express é 200 e get sempre 200 tb, mas vamos colocar so pra ficar mais claro no codigo
     resposta.send(
         JSON.stringify(resultados)
     )                                
@@ -16,6 +17,7 @@ roteador.post('/', async (requisicao, resposta) => {
         const dadosRecebidos = requisicao.body
         const fornecedor = new Fornecedor(dadosRecebidos)
         await fornecedor.criar() //espera criar fornecedor para realizar a função async
+        resposta.status(201)//status de 'criado'
         resposta.send(
             JSON.stringify(fornecedor)
         )
