59f120af5849fa9e9bee8c56f886fbae12103210
danielemaraschin <danikmaraschin@gmail.com>
2021-03-13 11:16:01 +0100

    Working with DELETE status 204

--- api-petshop/api/rotas/fornecedores/index.js
+++ api-petshop/api/rotas/fornecedores/index.js
@@ -71,6 +71,7 @@ roteador.delete('/:idFornecedor', async (requisicao, resposta) => {
             const fornecedor = new Fornecedor({ id: id}) //Instanciar a classe c/o unico parametro q temos desse obje é o id
             await fornecedor.carregar() //pegar id e verificar no db se existe
             await fornecedor.remover()
+            resposta.status(204)
             resposta.end() //finalizar a requisicao
     }catch(erro){ //se o id não existir
         resposta.send(       //objeto  com a propriedade mensagem para
