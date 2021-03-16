62de1a8a30360d2e8b70b6a5beb6e4d6d8111b5b
danielemaraschin <danikmaraschin@gmail.com>
2021-03-16 18:47:06 +0100

    working with error

--- api-petshop/api/index.js
+++ api-petshop/api/index.js
@@ -2,7 +2,7 @@ const express = require('express')      // precisamos usar o express para criar 
 const app = express() //precisamos de uma instancia do express
 const bodyParser = require('body-parser')
 const config = require('config')
-
+const NaoEncontrado = require('./erros/NaoEncontrado')
 
 //bodyParser é o plug-in para nosso app
 app.use(bodyParser.json())
@@ -10,6 +10,19 @@ app.use(bodyParser.json())
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador)
 
+app.use((erro, requisicao, resposta, proximo) => {
+    if(erro instanceof NaoEncontrado) { //erro é uma instancia de NAOENCONTRADO
+        resposta.status(404) //item nao encontrado ou nao existe   
+    } else{
+        resposta.status(400)
+    }
+    resposta.send(
+        JSON.stringify({
+            mensagem: erro.message,
+            id: erro.idErro
+        })
+    )
+})
             
 app.listen(config.get('api.porta'), () => console.log('A API está funcionando'))  //para a app funcionar ela precisa escutar alguma porta 
                       //P1-porta       p2- funcao callback                        //metodo listen aceita 2 parametros
