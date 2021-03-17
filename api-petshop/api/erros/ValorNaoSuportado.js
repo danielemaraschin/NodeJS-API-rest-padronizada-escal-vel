e94fe0b3c3538b2d70ad1c777b14fe1e730ec180
danielemaraschin <danikmaraschin@gmail.com>
2021-03-17 17:03:42 +0100

    working with error on serialization

---
+++ api-petshop/api/erros/ValorNaoSuportado.js
@@ -0,0 +1,10 @@
+class ValorNaoSuportado extends Error{
+
+    constructor (contentType) {
+        super(`o tipo de conteúdo '${contentType}' não é suportado`)
+        this.nome = ValorNaoSuportado
+        this.idErro = 3
+    }
+}
+
+module.exports = ValorNaoSuportado
