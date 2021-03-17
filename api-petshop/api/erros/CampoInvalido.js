950572d2c0de1cdf3f6ed1e3dd5cabccafeefd2f
danielemaraschin <danikmaraschin@gmail.com>
2021-03-17 11:11:13 +0100

    working with extends Error, module.exports, ...

--- api-petshop/api/erros/CampoInvalido.js
+++ api-petshop/api/erros/CampoInvalido.js
@@ -2,5 +2,9 @@ class CampoInvalido extends Error{ //CampoInvalido está herdando da classe ERRO
     constructor (campo) {
         const mensagem = `O campo '${campo}' está inválido`
         super(mensagem) //chamando o constructor da classe ERROR
+        this.name = 'CampoInvalido'
+        this.idErro = 1 //já que o NaoEncontrado é id = 0
     }
 }
+
+module.exports = CampoInvalido
