1fa611d33dfca946c5b613c32c18e65385619c47
danielemaraschin <danikmaraschin@gmail.com>
2021-03-17 11:31:43 +0100

    replacing throw new error

--- api-petshop/api/erros/CampoInvalido.js
+++ api-petshop/api/erros/CampoInvalido.js
@@ -1,9 +1,9 @@
-class CampoInvalido extends Error{ //CampoInvalido está herdando da classe ERROR
-    constructor (campo) {
+class CampoInvalido extends Error{                               //CampoInvalido está herdando da classe ERROR
+    constructor (campo) {   
         const mensagem = `O campo '${campo}' está inválido`
-        super(mensagem) //chamando o constructor da classe ERROR
+        super(mensagem)                                          //chamando o constructor da classe ERROR
         this.name = 'CampoInvalido'
-        this.idErro = 1 //já que o NaoEncontrado é id = 0
+        this.idErro = 1                                         //já que o NaoEncontrado é id = 0
     }
 }
 
