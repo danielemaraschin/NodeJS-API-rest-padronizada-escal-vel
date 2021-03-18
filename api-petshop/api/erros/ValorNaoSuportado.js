ee9620eb4c003d1f79e2d65840d8d51c5e1000e9
danielemaraschin <danikmaraschin@gmail.com>
2021-03-18 19:35:42 +0100

    working with error

--- api-petshop/api/erros/ValorNaoSuportado.js
+++ api-petshop/api/erros/ValorNaoSuportado.js
@@ -1,7 +1,7 @@
 class ValorNaoSuportado extends Error{
 
     constructor (contentType) {
-        super(`o tipo de conteúdo '${contentType}' não é suportado`)
+        super(`o tipo de conteúdo '${contentType}' não é suportado.`)
         this.nome = ValorNaoSuportado
         this.idErro = 3
     }
