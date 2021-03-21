ff2b7b6895edcc60ce554b0b777c6db69d33d737
danielemaraschin <danikmaraschin@gmail.com>
2021-03-21 16:24:46 +0100

    working with serialization on route GET

--- api-petshop/api/Serializador.js
+++ api-petshop/api/Serializador.js
@@ -14,7 +14,15 @@ class Serializador { //serializar (transformar) dados em json
     }
 }
 
+class SerializadorFornecedor extends Serializador {//quando instanciar a classe coloca no constructor
+    constructor (contentType) { //usa a var contentType para verficar o tipo de conteudo que tá aceitando por isso declarar no constructor
+        super()
+        this.contentType = contentType 
+
+    }
+}
 module.exports = {
     Serializador: Serializador,
+    SerializadorFornecedor: SerializadorFornecedor,
     formatosAceitos: ['application/json']
 }
