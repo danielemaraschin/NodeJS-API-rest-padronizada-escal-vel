e6d601e873252edc5cbe6ddaab2178107c4c82a6
danielemaraschin <danikmaraschin@gmail.com>
2021-03-23 13:47:04 +0100

    working with filter on serialization

--- api-petshop/api/Serializador.js
+++ api-petshop/api/Serializador.js
@@ -9,6 +9,7 @@ class Serializador { //serializar (transformar) dados em json
         if (this.contentType === 'application/json') { //verifica se o tipo de formato aceito é json
             return this.json(             //retorna os dados em json
                 this.filtrarObjetos(dados) //mas antes filtra pra retornar só os dados publicos
+            )
         }
                                                     //emite esse erro se o formato não é possível 
         throw new ValorNaoSuportado(this.contentType) //de ser transformado em json
