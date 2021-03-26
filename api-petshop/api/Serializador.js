3b24c5d32f0fac026e569a08dff1cd4c79420a5c
danielemaraschin <danikmaraschin@gmail.com>
2021-03-26 14:40:13 +0100

    working with xml serialization

--- api-petshop/api/Serializador.js
+++ api-petshop/api/Serializador.js
@@ -1,21 +1,25 @@
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 
 class Serializador { //serializar (transformar) dados em json 
-    json (dados) {
+    json(dados) {
         return JSON.stringify(dados)
     }
 
-    serializar (dados){         //formato aceito é o 'application/json'
+    xml(dados) { //
+
+    }
+    serializar(dados) {         //formato aceito é o 'application/json' e 'application/xml'
+        dados = this.filtrar(dados)
         if (this.contentType === 'application/json') { //verifica se o tipo de formato aceito é json
-            return this.json(             //retorna os dados em json
-                this.filtrar(dados) //mas antes filtra pra retornar só os dados publicos
-            )
+            return this.json(dados) //retorna dados em json
         }
-                                                    //emite esse erro se o formato não é possível 
-        throw new ValorNaoSuportado(this.contentType) //de ser transformado em json
+        if (this.contentType === 'application/xml') {
+            return this.xml(dados)
+        }
+        throw new ValorNaoSuportado(this.contentType) ////emite esse erro se o formato não é possível transformar em json
     }
 
-    filtrarObjeto (dados) { //quando listar os fornecedores não mostrar todos os dados;
+    filtrarObjeto(dados) { //quando listar os fornecedores não mostrar todos os dados;
         const novoObjeto = {} //declara objeto vazio
         //camposPublicos está no constructor do serializadorFornecedor
         this.camposPublicos.forEach((campo) => { //itera em cada campo verificando se sao os campos publicos       
@@ -26,12 +30,12 @@ class Serializador { //serializar (transformar) dados em json
         return novoObjeto //retorna um objeto filtrado so com os campos publicos
     }
 
-    filtrar (dados) {
+    filtrar(dados) {
         if (Array.isArray(dados)) {                  //se 'dados' for um array fazer essa função
-            dados = dados.map( item => {            //map é como o forEach que passa em cada item do array fazendo uma função e com o resultado dela cria um novo array
+            dados = dados.map(item => {            //map é como o forEach que passa em cada item do array fazendo uma função e com o resultado dela cria um novo array
                 return this.filtrarObjeto(item)         //a funcao filtrarObjetos já tem o forEach que processa esse objeto retornando um novo objeto com esse campos filtrados
             })
-            } else {                                        //se 'dados' não for array sobreescreve dados por this.filtraObjetos(dados)
+        } else {                                        //se 'dados' não for array sobreescreve dados por this.filtraObjetos(dados)
             dados = this.filtrarObjeto(dados)
         }
         return dados
@@ -40,23 +44,23 @@ class Serializador { //serializar (transformar) dados em json
 
 
 class SerializadorFornecedor extends Serializador {//quando instanciar a classe coloca no constructor
-    constructor (contentType, camposExtras) { //usa a var contentType para verficar o tipo de conteudo que tá aceitando por isso declarar no constructor
+    constructor(contentType, camposExtras) { //usa a var contentType para verficar o tipo de conteudo que tá aceitando por isso declarar no constructor
         super()
         this.contentType = contentType
         this.camposPublicos = [ //mostrar somente os dados/propriedades publicas
-            'id', 
-            'empresa', 
+            'id',
+            'empresa',
             'categoria'
         ].concat(camposExtras || []) //pra nao dar indefined em campos extras caso não seja atribuido nenhum valor, colocar || [] que é = OU uma lista vazia.
     }//esses campos extras são para a rota get/idFornecedor pq nessa rota queremos todos os detalhes do fornecedor, nao so os campos publicos
 }
 
 class SerializadorErro extends Serializador {
-    constructor (contentType, camposExtras){
+    constructor(contentType, camposExtras) {
         super()
         this.contentType = contentType
-        this.camposPublicos = [ 
-            'id', 
+        this.camposPublicos = [
+            'id',
             'mensagem'
         ].concat(camposExtras || [])
     }
